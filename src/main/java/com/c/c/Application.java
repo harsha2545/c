d21cf301eaa683cc4ea86a805a475d2c02a5c1f8
user1 <user1@gmail.com>
2019-06-30 07:23:30 +0000

    c

---
+++ src/main/java/com/c/c/Application.java
@@ -0,0 +1,15 @@
+package com.c.c;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.c.c.mapper")
+public class Application {
+
+	public static void main(String[] args)  {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
